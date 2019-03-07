181d8a052d07ba9f754b0dc9644da82e59987c72
vasleoni <vasleoni@192.168.56.1>
2019-03-07 10:10:55 +0100

    Prueba Git

---
+++ LuisCerrada/src/luiscerrada/LuisCerrada.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luiscerrada;
+
+/**
+ *
+ * @author vasleoni
+ */
+public class LuisCerrada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   int goles =3;
+    int partidos =1;
+        System.out.println("La cantidad de goles que lleva es " + goles + " en " + partidos + " partidos");
+    }
+    
+}
